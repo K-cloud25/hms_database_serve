faece1abd1dc424d3aae892c9918003cf4f3f1af
Keyur Oak <keyuroak.25@gmail.com>
2022-10-17 12:57:43 +0530

    change

--- src/files/Words.js
+++ src/files/Words.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react';
-import { json, useLocation,useNavigate } from 'react-router-dom';
+import { useLocation,useNavigate } from 'react-router-dom';
 
 
 function SingleItem(props){
